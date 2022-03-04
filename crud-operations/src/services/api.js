bd9eabbca78e6b3aa12652b5afc5173233e85493
Farhat-Khan12 <90671950+Farhat-Khan12@users.noreply.github.com>
2022-03-04 20:14:34 +0530

    Add files via upload

--- crud-operations/src/services/api.js
+++ crud-operations/src/services/api.js
@@ -1,1 +1,26 @@
+import axios from 'axios';
+const url = "http://localhost:3003/users"
+export const  GetTasks = async (id) => {
+    id = id || "";
+    return await (
+        axios.get(`${url}/${id}`)
+    );
+}
 
+export const addTask = async (task) => {
+    return await (
+        axios.post(url,task)
+    );
+}
+
+export const editTask = async(id,task)=>{
+    return await(
+        axios.put(`${url}/${id}`,task)
+    );
+}
+
+export const deleteTask = async(id)=>{
+    return await(
+        axios.delete(`${url}/${id}`)
+    );
+}
